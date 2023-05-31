b91ba5e1d071c12daae8cb5aefc6f2108936d778
lathief <shadowmaster241099@yahoo.co.id>
2023-06-01 02:28:17 +0700

    feat: call database

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/lathief/crm-service/utils/database"
+	"log"
+)
 
 func main() {
+	router := gin.New()
 	fmt.Println("Initial Project")
+	db, err := database.StartDB()
+	if err != nil {
+		return
+	}
+	checkdb, err := db.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ping to database
+	errconn := checkdb.Ping()
+	if err != nil {
+		log.Fatal(errconn)
+	}
+	errRouter := router.Run(":8080")
+	if errRouter != nil {
+		fmt.Printf(errRouter.Error())
+		return
+	}
 }
