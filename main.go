e86af04a43f09479332179d798a270434be36002
lathief <shadowmaster241099@yahoo.co.id>
2023-06-01 18:02:43 +0700

    feat: make config connection database

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/lathief/crm-service/config"
 	"github.com/lathief/crm-service/utils/database"
 	"log"
 )
+
+func init() {
+	config.SetupConfiguration()
+}
 
 func main() {
 	router := gin.New()
@@ -14,6 +19,7 @@ func main() {
 	if err != nil {
 		return
 	}
+	//check database
 	checkdb, err := db.DB()
 	if err != nil {
 		log.Fatal(err)
--- utils/database/db.go
+++ utils/database/db.go
@@ -2,32 +2,25 @@ package database
 
 import (
 	"fmt"
+	"github.com/lathief/crm-service/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 	"net/url"
 )
 
-var (
-	host       = "localhost"   //os.Getenv("DB_HOST")
-	user       = "root"        //os.Getenv("DB_USER")
-	password   = ""            //os.Getenv("DB_PASSWORD")
-	dbPort     = "3306"        //os.Getenv("DB_PORT")
-	dbName     = "crm_service" //os.Getenv("DB_NAME")
-	DEBUG_MODE = true
-	db         *gorm.DB
-	err        error
-)
-
 func StartDB() (*gorm.DB, error) {
 	val := url.Values{}
 	val.Add("parseTime", "True")
 	val.Add("loc", "Asia/Jakarta")
-	dsn := fmt.Sprintf(`%s:%s@tcp(%s:%v)/%s?%s`, user, password, host, dbPort, dbName, val.Encode())
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	//dsn := fmt.Sprintf(`%s:%s@tcp(%s:%v)/%s?%s`, user, password, host, dbPort, dbName, val.Encode())
+	dsn := fmt.Sprintf(`%s:%s@tcp(%s:%v)/%s?%s`, config.Config.Database.DBUser,
+		config.Config.Database.DBPass, config.Config.Database.DBHost, config.Config.Database.DBPort,
+		config.Config.Database.DBName, val.Encode())
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error connecting to database :", err)
+		return nil, err
 	}
 
 	fmt.Println("Connection database success")
