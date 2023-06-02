86f68db2da913e2f1eab09e3d7755e8c702c2eec
lathief <shadowmaster241099@yahoo.co.id>
2023-06-02 15:31:48 +0700

    feat: change type data to enum and name table

--- entity/actors.go
+++ entity/actors.go
@@ -1,11 +1,33 @@
 package entity
 
-type Actors struct {
+import "database/sql/driver"
+
+type boolType string
+
+const (
+	True  boolType = "true"
+	False boolType = "false"
+)
+
+func (ct *boolType) Scan(value interface{}) error {
+	*ct = boolType(value.([]byte))
+	return nil
+}
+
+func (ct boolType) Value() (driver.Value, error) {
+	return string(ct), nil
+}
+
+type Actor struct {
 	GormModel
 	Username   string
 	Password   string
 	RoleId     uint
-	IsVerified bool
-	IsActive   bool
+	IsVerified boolType
+	IsActive   boolType
 	Role       *Role
+}
+
+func (Actor) TableName() string {
+	return "actor"
 }
