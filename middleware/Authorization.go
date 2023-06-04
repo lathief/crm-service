7af23f55af0877cf5c41e3ecdffb1adc1f19e20a
lathief <shadowmaster241099@yahoo.co.id>
2023-06-04 12:42:43 +0700

    feat: make authentication and admin authorization

--- middleware/Authorization.go
+++ middleware/Authorization.go
@@ -2,14 +2,42 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/lathief/crm-service/payload/response"
 	"github.com/lathief/crm-service/repository"
 	"github.com/lathief/crm-service/utils/database"
 	"net/http"
+	"strconv"
 )
 
-func Authorization() gin.HandlerFunc {
+func AdminAuthorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db, err := database.StartDB()
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, response.HandleFailedResponse("Database Error "+err.Error(), http.StatusInternalServerError))
+			return
+		}
+		adminId, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, response.HandleFailedResponse("Invalid Path Variable "+err.Error(), http.StatusBadRequest))
+			return
+		}
+		adminData := c.MustGet("adminData").(jwt.MapClaims)
+		adminID := uint(adminData["id"].(float64))
+		ActorRepo := repository.ActorNewRepo(db)
+		_, err = ActorRepo.GetActorById(adminID)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, response.HandleFailedResponse("Account doesn't exist", http.StatusNotFound))
+			return
+		}
+		if uint(adminId) != adminID {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response.HandleFailedResponse("You are not allowed to access this data", http.StatusBadRequest))
+			return
+		}
+		c.Next()
+	}
+}
+func SuperAdminAuthorization() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		db, err := database.StartDB()
 		if err != nil {
@@ -19,9 +47,13 @@ func Authorization() gin.HandlerFunc {
 		adminData := c.MustGet("adminData").(jwt.MapClaims)
 		adminID := uint(adminData["id"].(float64))
 		ActorRepo := repository.ActorNewRepo(db)
-		_, err = ActorRepo.GetActorById(adminID)
+		spAdmin, err := ActorRepo.GetActorById(adminID)
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusNotFound, response.HandleFailedResponse("Account doesn't exist", http.StatusNotFound))
+			return
+		}
+		if spAdmin.Role.Rolename != "role_super_admin" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response.HandleFailedResponse("You are not allowed to access this data", http.StatusBadRequest))
 			return
 		}
 		c.Next()
